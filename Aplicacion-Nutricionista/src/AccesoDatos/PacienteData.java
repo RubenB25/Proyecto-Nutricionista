f98c25e825d20d739d5517f14ff553cc60a63b87
Gabriel Mina <gaabriiel.14@gmail.com>
2023-10-07 21:33:36 -0300

    Codigo agregado y modificado
    AlumnoData, Alumno, FormularioAlumno

--- Aplicacion-Nutricionista/src/AccesoDatos/PacienteData.java
+++ Aplicacion-Nutricionista/src/AccesoDatos/PacienteData.java
@@ -2,6 +2,7 @@ package AccesoDatos;
 
 import Entidades.Paciente;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,16 +22,21 @@ public class PacienteData {
     }
 
     public void guardarPaciente(Paciente paciente) {
-        String sql = "INSERT INTO `pacientes`( `nombre`, `apellido`, `dni`, "
-                + "`domicilio`, `celular`,estado) "
-                + "VALUES (?,?,?,?,?,?)";
+        String sql = "INSERT INTO pacientes (nombre, apellido, dni, "
+                + "domicilio, celular, fecha_nacimiento, edad, peso_actual, peso_deseado, estado) "
+                + "VALUES (?,?,?,?,?,?,?,?,?,1)";
         try {
             PreparedStatement ps = conex.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, paciente.getNombre());
             ps.setString(2, paciente.getApellido());
             ps.setInt(3, paciente.getDni());
             ps.setString(4, paciente.getDomicilio());
-            ps.setInt(5, paciente.getCelular());
+            ps.setString(5, paciente.getCelular());
+            ps.setDate(6, Date.valueOf(paciente.getFechaNac()));
+            ps.setString(7, paciente.getEdad());
+            ps.setDouble(8, paciente.getPesoActual());
+            ps.setDouble(9, paciente.getPesoDeseado());
+            ps.setBoolean(10, paciente.isEstado());
             ps.executeUpdate();
             ResultSet resultado = ps.getGeneratedKeys();
             if (resultado.next()) {
@@ -60,7 +66,7 @@ public class PacienteData {
                 paciente.setDni(resultado.getInt("dni"));
                 paciente.setApellido(resultado.getString("apellido"));
                 paciente.setNombre(resultado.getString("nombre"));
-                paciente.setCelular(resultado.getInt("celular"));
+                paciente.setCelular(resultado.getString("celular"));
                 paciente.setEstado(resultado.getBoolean("estado"));
             } else {
                 JOptionPane.showMessageDialog(null, "No existe el Paciente");
@@ -80,7 +86,7 @@ public class PacienteData {
             ps.setString(1, paciente.getNombre());
             ps.setString(2, paciente.getApellido());
             ps.setInt(3, paciente.getDni());
-            ps.setInt(4, paciente.getCelular());
+            ps.setString(4, paciente.getCelular());
             ps.setBoolean(5, paciente.isEstado());
             ps.setInt(6, paciente.getIdPaciente());
             int exito = ps.executeUpdate();
@@ -120,12 +126,12 @@ public class PacienteData {
             ResultSet rs = psa.executeQuery();
             while (rs.next()) {
                 Paciente paciente = new Paciente();
-                
+
                 paciente.setIdPaciente(rs.getInt("id_paciente"));
                 paciente.setNombre(rs.getString("nombre"));
                 paciente.setApellido(rs.getString("apellido"));
                 paciente.setDni(rs.getInt("dni"));
-                paciente.setCelular(rs.getInt("celular"));
+                paciente.setCelular(rs.getString("celular"));
                 paciente.setEstado(rs.getBoolean("estado"));
                 pacientes.add(paciente);
             }
